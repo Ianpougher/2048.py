8d4b4693e22bec22d495c754d113a6e901b3bbbc
Ian Pougher <ianpougher@gmail.com>
2017-01-21 00:25:17 +0000

    Change Controls to IJKL movement format

--- 2048.py
+++ 2048.py
@@ -42,10 +42,10 @@ def get_new_field():
 def tilt_field(f, cmd):
     f2=get_new_field()
     for n in range(4):
-        if cmd=="w":liste=getnums(f,c=[n])
-        if cmd=="a":liste=getnums(f,r=[n])
-        if cmd=="s":liste=getnums(f,c=[n])[::-1]
-        if cmd=="d":liste=getnums(f,r=[n])[::-1]
+        if cmd=="i":liste=getnums(f,c=[n])
+        if cmd=="j":liste=getnums(f,r=[n])
+        if cmd=="k":liste=getnums(f,c=[n])[::-1]
+        if cmd=="l":liste=getnums(f,r=[n])[::-1]
         start_len=len(liste)
         while 0 in liste:
             liste.remove(0)
@@ -55,10 +55,10 @@ def tilt_field(f, cmd):
                 liste[i] = liste[i] * 2
             i = i + 1
         liste += [0] * (start_len - len(liste))
-        if cmd=="w": liste=sert(f2,liste, c=[n])
-        if cmd=="a": liste=sert(f2,liste, r=[n])
-        if cmd=="s": liste=sert(f2,liste, c=[n], reverse = True)
-        if cmd=="d": liste=sert(f2,liste, r=[n], reverse = True)
+        if cmd=="i": liste=sert(f2,liste, c=[n])
+        if cmd=="j": liste=sert(f2,liste, r=[n])
+        if cmd=="k": liste=sert(f2,liste, c=[n], reverse = True)
+        if cmd=="l": liste=sert(f2,liste, r=[n], reverse = True)
     return f2
 
 def max_tile(f):
@@ -82,11 +82,11 @@ def pprint(f):
     
 
 f=add_random(add_random(get_new_field()))
-while not (f==tilt_field(f,"w")==tilt_field(f,"a")
-==tilt_field(f,"s")==tilt_field(f,"d")):
+while not (f==tilt_field(f,"i")==tilt_field(f,"j")
+==tilt_field(f,"k")==tilt_field(f,"l")):
     cmd=""
     pprint(f)
-    while cmd not in["w","a","s","d"]:
+    while cmd not in["i","j","k","l"]:
         cmd=raw_input(">")
     f2=tilt_field(f,cmd)
     if f2!=f:f=add_random(f2)
